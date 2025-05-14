4ffadece39b96430ebacfb2d890a639d8d0c11c1
Erfan Ali Yavari <erfanaliyavari@bitpardaz.ir>
2025-05-14 13:53:15 +0330

    fixed report charts

--- app/reports.py
+++ app/reports.py
@@ -2,6 +2,8 @@ from slick_reporting.views import ReportView, Chart
 from slick_reporting.fields import ComputationField
 from django.db.models import Count, Sum, Avg
 from .models import Author, Book, Purchase
+
+
 class BookSalesReport(ReportView):
     report_model = Purchase
     date_field = "purchased_at"
@@ -11,14 +13,16 @@ class BookSalesReport(ReportView):
         "book__author__first_name",
         "book__author__last_name",
         "book__category__title",
-        ComputationField.create(Sum, "price", verbose_name="Total Sales ($)"),
-        ComputationField.create(Count, "id", verbose_name="Number of Purchases"),
+        ComputationField.create(Sum, "price", name="price__sum", verbose_name="Total Sales ($)"),
+        ComputationField.create(Count, "id", name="id__count", verbose_name="Number of Purchases"),
     ]
 
     chart_settings = [
-        Chart("Total Sales by Book", Chart.BAR, data_source=["price__sum"], title_source=["book__title"]),
         Chart("Number of Purchases by Book", Chart.PIE, data_source=["id__count"], title_source=["book__title"]),
+        Chart("Total Sales by Book", Chart.PIE, data_source=["price__sum"], title_source=["book__title"]),
+
     ]
+
 
 class BookPublicationReport(ReportView):
     report_model = Book
@@ -26,8 +30,8 @@ class BookPublicationReport(ReportView):
     group_by = "publication_year"
     columns = [
         "publication_year",
-        ComputationField.create(Sum, "price", verbose_name="Total Price ($)"),
-        ComputationField.create(Count, "id", verbose_name="Number of Books"),
+        ComputationField.create(Sum, "price", name="price__sum", verbose_name="Total Price ($)"),
+        ComputationField.create(Count, "id", name="id__count", verbose_name="Number of Books"),
     ]
 
     chart_settings = [
@@ -35,26 +39,29 @@ class BookPublicationReport(ReportView):
         Chart("Books Published per Year", Chart.BAR, data_source=["id__count"], title_source=["publication_year"]),
     ]
 
+
+
 class AuthorByCountryReport(ReportView):
     report_model = Author
     group_by = "country__name"  # Group authors by their country name
     columns = [
         "country__name",
-        ComputationField.create(Count, "id", verbose_name="Number of Authors"),
-        ComputationField.create(Avg, "age", verbose_name="Average Age"),
+        ComputationField.create(Count, "id", name="author_count", verbose_name="Number of Authors"),
+        ComputationField.create(Avg, "age", name="average_age", verbose_name="Average Age"),
     ]
 
     chart_settings = [
         Chart(
             "Number of Authors by Country",
             Chart.BAR,
-            data_source=["id__count"],
+            data_source=["author_count"],
             title_source=["country__name"],
         ),
         Chart(
             "Average Age of Authors by Country",
             Chart.LINE,
-            data_source=["age__avg"],
+            data_source=["average_age"],
             title_source=["country__name"],
         ),
     ]
+
